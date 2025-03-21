c0fffe471f0a40e74309d6b97037e4d517020355
Melker Andreasson <ma225tq@student.lnu.se>
2025-03-21 15:43:07 +0100

    add docker compose

--- backend/app/app.py
+++ backend/app/app.py
@@ -65,5 +65,4 @@ print('REMEMBER TO USE THE AGENT WITH APPROPRIATE PERMISSIONS!!!!')
 print('REMEMBER TO USE THE AGENT WITH APPROPRIATE PERMISSIONS!!!!')
 print('REMEMBER TO USE THE AGENT WITH APPROPRIATE PERMISSIONS!!!!')
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
