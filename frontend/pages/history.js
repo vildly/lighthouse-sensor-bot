75a28c7caadf47de9c10c78f66a81417125b17e3
Melker Andreasson <ma225tq@student.lnu.se>
2025-04-14 14:58:30 +0200

    change column name Actions to Full Response in on history page

--- frontend/pages/history.js
+++ frontend/pages/history.js
@@ -215,7 +215,7 @@ export default function HistoryPage() {
                       )}
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Actions
+                      Full Response
                     </th>
                   </tr>
                 </thead>
