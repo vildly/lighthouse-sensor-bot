bfec88a1bafab264d790a3803d83f7976fc03bbf
Melker Andreasson <ma225tq@student.lnu.se>
2025-04-14 14:22:40 +0200

    add query history page to frontend

--- frontend/components/navigation.js
+++ frontend/components/navigation.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Navigation() {
   const { connected } = useWebSocket();
-  
+
   return (
     <header className="pt-4">
       <div className="arc-navbar">
@@ -40,6 +40,14 @@ export default function Navigation() {
                     <path d="M6 12a1 1 0 011-1h2a1 1 0 110 2H7a1 1 0 01-1-1z" />
                   </svg>
                   Analytics
+                </a>
+              </Link>
+              <Link href="/history">
+                <a className="text-white hover:text-gray-300 transition-colors font-medium flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
+                  </svg>
+                  History
                 </a>
               </Link>
               <div className="flex items-center">
