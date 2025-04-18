6c6d971cd7d5158e1974f4014bf8dd23d81ba9ad
Melker Andreasson <ma225tq@student.lnu.se>
2025-04-18 14:46:28 +0200

    use Gunicorn production server in backend Dockerfile

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# lighthouse
+# lighthouse bot
 
 ## Overview
 
---
+++ backend/gunicorn_config.py
@@ -0,0 +1,4 @@
+bind = "0.0.0.0:5000"  # Bind to all network interfaces on this port
+workers = 4  # Number of worker processes
+timeout = 120  # Request timeout in seconds
+loglevel = "info"  # Logging level
