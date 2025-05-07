7425638934b2d5b23fc6cd184cf72868be0ab36b
Dong <dt222ha@student.lnu.se>
2025-05-07 08:23:39 +0200

    Updated to Gemini 2.5 Flash Preview

--- backend/postgres/init/insert_models.sql
+++ backend/postgres/init/insert_models.sql
@@ -1,6 +1,6 @@
 INSERT INTO llm_models (name, type) VALUES ('openai/gpt-4o-2024-11-20', 'proprietary');
 INSERT INTO llm_models (name, type) VALUES ('anthropic/claude-3.7-sonnet', 'proprietary');
-INSERT INTO llm_models (name, type) VALUES ('google/gemini-2.0-flash-001', 'proprietary');
+INSERT INTO llm_models (name, type) VALUES ('google/gemini-2.5-flash-preview', 'proprietary');
 INSERT INTO llm_models (name, type) VALUES ('amazon/nova-pro-v1', 'proprietary');
 INSERT INTO llm_models (name, type) VALUES ('qwen/qwen-2.5-72b-instruct', 'open source');
 INSERT INTO llm_models (name, type) VALUES ('meta-llama/llama-3.3-70b-instruct', 'open source');
